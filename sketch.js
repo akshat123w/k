2e091b275a21de8631262ad8d19e12954d4092ce
akshat123w <86547650+akshat123w@users.noreply.github.com>
2021-08-09 11:29:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function preload(){
+  //pre-load images
+bombImg= addImage();  
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+}
+
+function draw() {
+  background(0);
+
+}
